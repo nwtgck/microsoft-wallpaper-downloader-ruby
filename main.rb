cabddd6554d15841f2337ceab65bc632dd8284d2
Ryo Ota <nwtgck@gmail.com>
2018-03-01 20:12:37 +0900

    [change] Adapt concurrent download

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require 'open-uri'
+require 'thread'
 
 OUT_DIR_PATH = "wallpapers"
+semaphore_num = 10 # TODO: Hard code
 
 # If no directory
 if !Dir.exists?(OUT_DIR_PATH)
@@ -8,20 +10,31 @@ if !Dir.exists?(OUT_DIR_PATH)
     Dir.mkdir(OUT_DIR_PATH)
 end
 
+# for counting semaphore (from: https://qiita.com/na-o-ys/items/62e9abc21c236908c958#_reference-d496b8cc1d1f2cc3e9e8)
+locks = Queue.new
+semaphore_num.times { locks.push :lock }
 
-(19000...19987).each{|img_num| # TODO: Hard code
-    # Get image URL
-    img_url = "https://kbdevstorage1.blob.core.windows.net/asset-blobs/#{img_num}_en_1"
+(19000...19987).map{|img_num| # TODO: Hard code
+    Thread.new do
+        # for counting semaphore
+        lock = locks.pop
 
-    # Out file name
-    out_file_name = "wallpaper#{img_num}.jpg"
+        # Get image URL
+        img_url = "https://kbdevstorage1.blob.core.windows.net/asset-blobs/#{img_num}_en_1"
 
-    puts("Downloading #{img_num} ...")
+        # Out file name
+        out_file_name = "wallpaper#{img_num}.jpg"
 
-    # Save a wallpaper as an image
-    IO.copy_stream(
-        open(img_url),
-        open(File.join(OUT_DIR_PATH, out_file_name), 'wb')
-    )
-}
+        # Save a wallpaper as an image
+        IO.copy_stream(
+            open(img_url),
+            open(File.join(OUT_DIR_PATH, out_file_name), 'wb')
+        )
+
+        puts("#{img_num} Done!")
+        
+        # for counting semaphore
+        locks.push lock 
+    end
+}.each(&:join)
 
