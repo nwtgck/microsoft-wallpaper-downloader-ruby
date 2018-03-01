64de3648c2f1d0f3efbf1b431491f4e4275474a6
Ryo Ota <nwtgck@gmail.com>
2018-03-01 20:53:35 +0900

    [change] Specify values by options

--- main.rb
+++ main.rb
@@ -1,13 +1,41 @@
+require 'optparse'
 require './ms_wallpaper_downloader'
 
-OUT_DIR_PATH  = "wallpapers" # TODO: Hard code
-semaphore_num = 10 # TODO: Hard code
-from_img_num  = 19000 # TODO: Hard code
-to_img_num    = 19987 # TODO: Hard code
+options = {
+    outdir_path:   "wallpapers",
+    semaphore_num: 10,
+    from_img_num:  19000,
+    to_img_num:    19987
+}
+OptionParser.new do |opts|
+  opts.banner = "Usage: bundle exec ruby main.rb [options]"
+
+  opts.on("--outdir=DIR_PATH", String, "Path of output directory (default: #{options[:outdir_path].inspect})") do |v|
+    options[:outdir_path] = v
+  end
+
+  opts.on("--semaphore-num=NUM", Integer, "The number of semaphores for concurrent download (default: #{options[:semaphore_num].inspect})") do |v|
+    options[:semaphore_num] = v
+  end
+
+  opts.on("--from-num=NUM", Integer, "From-image number (default: #{options[:from_img_num].inspect})") do |v|
+    options[:from_img_num] = v
+  end
+    
+  opts.on("--to-num=NUM", Integer, "To-image number (default: #{options[:to_img_num].inspect})") do |v|
+    options[:to_img_num] = v
+  end
+
+end.parse!
+
+outdir_path   = options[:outdir_path]
+semaphore_num = options[:semaphore_num]
+from_img_num  = options[:from_img_num]
+to_img_num    = options[:to_img_num]
 
 # Download wallpapers
 MSWallpaperDownloader::download(
-    OUT_DIR_PATH,
+    outdir_path,
     semaphore_num,
     from_img_num,
     to_img_num
